f39cb83b32c8618c27f40cab2dd3d545f053420e
Amrut Jadhav <amrutjadhav@users.noreply.github.com>
2016-04-29 01:30:56 +0530

    Keys change

--- wordcloud.R
+++ wordcloud.R
@@ -2,10 +2,10 @@ library("wordcloud")
 library("tm")
 library(httr)
 library(twitteR)
-api_key='254QuM8bwXiCq6MNYw65HlDCE'
-api_secret='9ACTyUv4eeTd6WG1GidCHvxpr25mDHO3luKjnmTq4tuQfGjChQ'
-access_token='2874875581-B6mI6oP6BsMNBgYLAoSopu7fDAioHw3MzWckJPB'
-access_token_secret='pZ1dV20DZhNjqQX18dAOzxfumEQP8UpOw5lQOq10KU4h8'
+api_key='YOUR_APP_API_KEY'
+api_secret='YOUR_APP_API_SECRET_KEY'
+access_token='YOUR_APP_ACCESS_TOKEN'
+access_token_secret='YOUR_APP_ACCESS_TOKEN_SECRET'
 setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
 tweets <- searchTwitter('#success', n=150) 
 #tweets.df <- do.call(rbind, lapply(tweets, as.data.frame))
